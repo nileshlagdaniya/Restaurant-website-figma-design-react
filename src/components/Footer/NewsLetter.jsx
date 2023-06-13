b5501531fc206615db51f5a8effda81a93fe9d44
NILESH LAGDANIYA <nileshlagadaniya@gmail.com>
2023-06-13 11:46:23 +0530

    first commit

---
+++ src/components/Footer/NewsLetter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./newsLetter.css";
+const NewsLetter = () => {
+  return <div>NewsLetter</div>;
+};
+
+export default NewsLetter;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SubHeading from "./SubHeading/SubHeading"
+import NewsLetter from "./NewsLetter/NewsLetter.jsx"
+import Navbar from "./Navbar/Navbar"
+import FooterOverlay from "./Footer/FooterOverlay"
+import MenuItem from "./MenuItem/MenuItem"
+
+export { SubHeading, Navbar, NewsLetter, FooterOverlay, MenuItem } 
